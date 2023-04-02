360b7acbf861b01dba45da929d71ec30eee8e84e
bruno <brunodecruz.05@gmail.com>
2023-04-02 15:14:21 -0300

    v2.0.0

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,40 +1,71 @@
-cantPeces =  int(input("Ingrese y0: "))
-pescaDiaria =  int(input("Ingrese X: "))
-tasaRepro =  float(input("Ingrese alfa: "))
-capacidadLago =  int(input("Ingrese beta: "))
-proporcionDepredadores =  float(input("Ingrese gama: "))
-cantDias =  int(input("Ingrese N: "))
+# función para pedir un valor numérico al usuario y manejar posibles errores
+def pedirNumero(mensaje, tipo):
+    while True:
+        valor = input(mensaje)
+        try:
+            valor = tipo(valor)
+            return valor
+        except ValueError:
+            if tipo==int:
+                print("Error: el valor ingresado debe ser un número entero.")
+            else:
+                print("Error: el valor ingresado debe ser un número")
 
-def poblacionDiaria (cantPecesXdia):
-    resultado  = cantPecesXdia+tasaRepro*cantPecesXdia*(capacidadLago-cantPecesXdia)-proporcionDepredadores*cantPecesXdia-pescaDiaria
-    if resultado <0:
+# función para calcular la población en un día determinado basándose en la población actual
+def poblacionDiaria(cantPecesXdia, tasaRepro, capacidadLago, proporcionDepredadores, pescaDiaria):
+    resultado = cantPecesXdia + tasaRepro * cantPecesXdia * (capacidadLago - cantPecesXdia) - proporcionDepredadores * cantPecesXdia - pescaDiaria
+    # Asegurarse de que la población no sea inferior a 0 ni superior a la población máxima permitida
+    if resultado < 0:
         return 0 
+    elif resultado > 50000:
+        return 50000
     return resultado
 
-def calcularPoblacion (cantPeces):
-    if cantPeces >50000:
-        print("Valor Y0 no puede ser mayor a la cantidad maxima permitida")
-        return
-    if cantPeces <0 :
-        print("Valor Y0 no puede ser negativa")
-        return
-    if tasaRepro <0:
-        print("La tasa de reproduccion no puede ser menor a 0 ")
-        return
-    if proporcionDepredadores <0:
-        print("La proporcion de peces comidos no puede ser menor a 0 ")
-        return
-    if pescaDiaria <0:
-        print("La cantidad de pesa diaria no puede ser menor a 0")
-        return
-    print("""Tabla de valores:
-t_i| y_i
--------------
-0  |  """+str(cantPeces))
-    for dia in range(cantDias):
-        poblacion = poblacionDiaria(cantPeces)
-        print(" "+ str(dia+1) + " | " +str(int(poblacion)))
-        cantPeces= poblacion 
-    return     
+# función para calcular la población a lo largo de varios días
+def calcularPoblacion():
+    # Pedir los valores de entrada al usuario y manejar posibles errores
+    
+    cantPeces = pedirNumero ("Ingrese y0: ", int)
+    while cantPeces > 50000 or cantPeces < 0:
+        print("Error: la cantidad de peces no puede ser mayor a 50000 ni menor a 0.")
+        cantPeces = pedirNumero ("Ingrese y0: " , int)
+    
+    pescaDiaria = pedirNumero ("Ingrese X: " ,float)
+    while pescaDiaria < 0:
+        print("Error: la cantidad de pesca diaria no puede ser menor a 0.")
+        pescaDiaria = pedirNumero ("Ingrese X: ", float)
+        
+    tasaRepro = pedirNumero("Ingrese alfa: ", float)
+    while tasaRepro < 0:
+        print("Error: la tasa de reproducción no puede ser menor a 0.")
+        tasaRepro = pedirNumero("Ingrese alfa: ", float)
+    
+    capacidadLago = pedirNumero ("Ingrese beta: ", int)
+    while capacidadLago > 50000 or capacidadLago < 0:
+        print("Error: la capacidad del lago no puede ser mayor a 50000 ni menor a 0.")
+        capacidadLago = pedirNumero ("Ingrese Beta: " , int)
+    
+    proporcionDepredadores = pedirNumero("Ingrese gama: " , float)
+    while proporcionDepredadores < 0:
+        print("Error: la proporción de peces comidos por depredadores no puede ser menor a 0.")
+        proporcionDepredadores = pedirNumero("Ingrese gama: ", float)
+    
+    cantDias = pedirNumero ("Ingrese N: " , int)
+    while  cantDias < 0:
+        print("Error: La cantida de dias no puede ser menor a 0")
+        cantDias = pedirNumero ("Ingrese N: ", int)
 
-calcularPoblacion( cantPeces)
+    print("""Tabla de valores:
+t_i | y_i
+-------------
+0  |  """ + str(cantPeces))
+
+    # Iterar sobre el número especificado de días y calcular la población para cada día
+    for dia in range(cantDias):
+        poblacion = poblacionDiaria(cantPeces, tasaRepro, capacidadLago, proporcionDepredadores, pescaDiaria)
+        print(" " + str(dia + 1) + " | " + str(int(poblacion)))
+        cantPeces = poblacion
+
+    return
+
+calcularPoblacion()
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,32 +1,41 @@
-cantPeces = 24487*0.9
-TasaRepro= 0.000082
-CapacidadLago=24487
-DepreProporciones= 0.1
-pescaDiaria=0
-cantDias=90
-def poblacionDiaria (cantPecesXdia, pescaDiariaFuncion):
-    resultado  = cantPecesXdia+TasaRepro*cantPecesXdia*(CapacidadLago-cantPecesXdia)-DepreProporciones*cantPecesXdia-pescaDiariaFuncion
-    if resultado <0:
+cantPeces = 24487*0.9 
+TasaRepro = 0.000082 
+CapacidadLago = 24487 
+DepreProporciones = 0.1 
+pescaDiaria = 0 
+cantDias = 90 
+
+def poblacionDiaria(cantPecesXdia, pescaDiariaFuncion):
+    # función que calcula la población diaria de peces
+    resultado = cantPecesXdia + TasaRepro * cantPecesXdia * (CapacidadLago - cantPecesXdia) - DepreProporciones * cantPecesXdia - pescaDiariaFuncion
+    if resultado < 0:
         return 0 
+    elif resultado > 50000:
+        return 50000
     return resultado
 
 
-def poblacion90Dias(cantPeces,pescaDiariaFuncion):
+def poblacion90Dias(cantPeces, pescaDiariaFuncion):
+    # función que calcula la población de peces durante 90 días
     for dia in range(cantDias):
-            poblacion = poblacionDiaria(cantPeces,pescaDiariaFuncion)
-            cantPeces= poblacion 
-            if cantPeces == 0:
-                break
+        poblacion = poblacionDiaria(cantPeces, pescaDiariaFuncion)
+        cantPeces = poblacion 
+        if cantPeces == 0:
+            break
     return poblacion
 
-def calcularPoblacion (cantPeces,pescaDiariaFuncion):
-    while True :    
-        resultado = poblacion90Dias( cantPeces,pescaDiariaFuncion)
-        if resultado >0 :
-            maximoPesca= pescaDiariaFuncion
-            maximoPoblacion= int(resultado)
-            pescaDiariaFuncion +=1
+
+def calcularPoblacion(cantPeces, pescaDiariaFuncion):
+    # función que encuentra el máximo de pesca diaria permitido sin reducir la población de peces por debajo de cero
+    while True:    
+        resultado = poblacion90Dias(cantPeces, pescaDiariaFuncion)
+        if resultado > 0:
+            maximoPesca = pescaDiariaFuncion
+            maximoPoblacion = int(resultado)
+            pescaDiariaFuncion += 1
         else:
             break    
-    print("Un Maximo de Pesca de " +  str( maximoPesca) + " con una poblacion de " + str(maximoPoblacion ))
-calcularPoblacion( cantPeces,pescaDiaria)
+    print("Un Maximo de Pesca de " + str(maximoPesca) + " con una poblacion de " + str(maximoPoblacion))
+
+
+calcularPoblacion(cantPeces, pescaDiaria)
